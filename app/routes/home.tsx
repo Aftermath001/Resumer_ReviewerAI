a1d859d0a7447f130f61c34f971b7353091a1c3d
Aftermath001 <alvinasiachi@gmail.com>
2025-08-27 23:05:18 +0300

    HomePage Navbar ResumeCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <Navbar />
     <section className="main-section">
       <div className="page-heading py-16">
-        <h1>Track your Resume & Application Ratings</h1>
+        <h1>Track your Resume & Application  Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
 
